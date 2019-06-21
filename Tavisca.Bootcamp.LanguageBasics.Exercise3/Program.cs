428b4f15b3b0eb233ec7ae09d77d4f10339b8f00
Venom <saurabhnarhe123@gmail.com>
2019-06-21 18:40:47 +0530

    code completed

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-
+using System.Collections.Generic;
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
@@ -38,9 +38,119 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine(result);
         }
 
+        public static int findMax(int[] list, List<int> indexes) {
+            
+            if(indexes.Count == 1) 
+                return list[indexes[0]];
+            
+            int max = list[indexes[0]];
+            
+            for(int i=0; i<indexes.Count; i++) 
+                if(max < list[indexes[i]]) 
+                    max = list[indexes[i]];
+            
+            return max;
+        
+        }
+        
+        public static int findMin(int[] list, List<int> indexes) {
+        
+            if (indexes.Count == 1)
+                return list[indexes[0]];
+            
+            int min = list[indexes[0]];
+
+            for (int i = 0; i < indexes.Count; i++)
+                if (min > list[indexes[i]])
+                    min = list[indexes[i]];
+            
+            return min;
+       
+        }
+
+        public static List<int> updateIndexes(int[] list, List<int> indexes, int val) {
+           
+            List<int> updatedIndexes = new List<int>();
+            
+            for(int i=0; i<list.Length; i++) 
+                if(list[i] == val) 
+                    updatedIndexes.Add(i);
+            
+            return updatedIndexes;
+        
+        }
+
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
+            
+            int[] cal = new int[protein.Length];
+            int[] meals = new int[dietPlans.Length];
+            
+            for(int i =0; i<protein.Length; i++) 
+                cal[i] = fat[i]*9 + (carbs[i]+protein[i])*5;
+
+            // Console.WriteLine($"Calories = [{string.Join(", ", cal)}]");            
+            
+            for(int i=0; i<dietPlans.Length; i++) {
+                
+                List<int> indexes = new List<int>();
+                int max, min;
+                
+                for(int k=0; k<protein.Length; k++) 
+                    indexes.Add(k);
+
+                for(int j=0;j<dietPlans[i].Length; j++) {
+
+                    
+                    switch(dietPlans[i][j]) {    
+
+                        case 'C':
+                            max = findMax(carbs, indexes);
+                            indexes = updateIndexes(carbs, indexes, max);
+                            break;
+
+                        case 'c':
+                            min = findMin(carbs, indexes);
+                            indexes = updateIndexes(carbs, indexes, min);
+                            break;
+                        
+                        case 'P':
+                            max = findMax(protein, indexes);
+                            indexes = updateIndexes(protein, indexes, max);
+                            break;
+                        
+                        case 'p':
+                            min = findMin(protein, indexes);
+                            indexes = updateIndexes(protein, indexes, min);
+                            break;
+                        
+                        case 'F':
+                            max = findMax(fat, indexes);
+                            indexes = updateIndexes(fat, indexes, max);
+                            break;
+                        
+                        case 'f':
+                            min = findMin(fat, indexes);
+                            indexes = updateIndexes(fat, indexes, min);
+                            break;
+                        
+                        case 'T':
+                            max = findMax(cal, indexes);
+                            indexes = updateIndexes(cal, indexes, max);
+                            break;
+                        
+                        case 't':
+                            min = findMin(cal, indexes);
+                            indexes = updateIndexes(cal, indexes, min);
+                            break;
+                    }
+                }
+                meals[i] = indexes[0];
+
+            }
+            
+            return meals;
+
             throw new NotImplementedException();
         }
     }
